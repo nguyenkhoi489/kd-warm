3290527bedb4b290e8b4c6dadc7d561825fc614c
Nguyên Khôi <vanngelttt@gmail.com>
2026-06-30 12:35:56 +0700

    test: pin serverEngine decode-default and frozen raw values

---
+++ KTStackKitTests/SiteServerEngineTests.swift
@@ -0,0 +1,30 @@
+import XCTest
+@testable import KTStackKit
+
+final class SiteServerEngineTests: XCTestCase {
+    func testEngineRawValuesAreFrozen() {
+        XCTAssertEqual(WebServerEngine.nginx.rawValue, "nginx")
+        XCTAssertEqual(WebServerEngine.apache.rawValue, "apache")
+    }
+
+    func testPreUpgradeJSONDecodesToNginx() throws {
+        let json = """
+        {"name":"demo","path":"/s","docroot":"/s/public","domain":"demo.test","phpVersion":"8.4","type":"php"}
+        """
+        let site = try JSONDecoder().decode(Site.self, from: Data(json.utf8))
+        XCTAssertEqual(site.serverEngine, .nginx)
+    }
+
+    func testEngineRoundTrips() throws {
+        var site = Site(name: "demo", path: "/s", docroot: "/s/public", domain: "demo.test", phpVersion: "8.4", type: .php)
+        site.serverEngine = .apache
+        let data = try JSONEncoder().encode(site)
+        let decoded = try JSONDecoder().decode(Site.self, from: data)
+        XCTAssertEqual(decoded.serverEngine, .apache)
+    }
+
+    func testDefaultEngineIsNginx() {
+        let site = Site(name: "demo", path: "/s", docroot: "/s/public", domain: "demo.test", phpVersion: "8.4", type: .php)
+        XCTAssertEqual(site.serverEngine, .nginx)
+    }
+}
